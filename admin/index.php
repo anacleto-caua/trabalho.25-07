935c8dff511f8f4def7fc688a6fe25b8f04375e6
anacleto-caua <anacleto.for.work@gmail.com>
2021-07-22 11:47:56 -0300

    novas pastas em admin

--- admin/index.php
+++ admin/index.php
@@ -14,18 +14,61 @@
 <body>
     <?php
         include "assets/iframes/header.html";
-        include "assets/iframes/nav.html";
+        include "assets/iframes/nav_ex.html";
     ?>
 
     <section>
 
+        <h1>Lorem Ipsum</h1>
+        <h2>Lorem Ipsum</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem voluptatem aspernatur ratione temporibus voluptatum quod optio architecto magnam corrupti quibusdam. Excepturi mollitia est vitae magni maxime expedita minima sed eveniet.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias excepturi nulla earum ipsa exercitationem quod. Sit sint quidem, aspernatur tempore id in facilis possimus necessitatibus fuga exercitationem architecto neque mollitia?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veniam dolor consectetur sit consequatur iusto nostrum a quis corrupti culpa corporis, minus dolorem excepturi ipsam reprehenderit maiores illo mollitia reiciendis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui culpa sunt vero ad quaerat tempora, a nostrum. Illum, ea voluptate soluta eum corporis nam labore obcaecati quos aliquam repellendus deserunt.
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut qui laudantium esse voluptas earum est perferendis, dolor ad dolore laboriosam, labore veniam quibusdam molestiae fugiat vel alias possimus, animi officiis!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti modi vitae accusamus excepturi culpa ex nihil recusandae, dolores similique voluptate quae quidem corporis atque eos. Nesciunt minus perspiciatis beatae ipsa?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem voluptatem aspernatur ratione temporibus voluptatum quod optio architecto magnam corrupti quibusdam. Excepturi mollitia est vitae magni maxime expedita minima sed eveniet.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias excepturi nulla earum ipsa exercitationem quod. Sit sint quidem, aspernatur tempore id in facilis possimus necessitatibus fuga exercitationem architecto neque mollitia?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veniam dolor consectetur sit consequatur iusto nostrum a quis corrupti culpa corporis, minus dolorem excepturi ipsam reprehenderit maiores illo mollitia reiciendis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui culpa sunt vero ad quaerat tempora, a nostrum. Illum, ea voluptate soluta eum corporis nam labore obcaecati quos aliquam repellendus deserunt.
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut qui laudantium esse voluptas earum est perferendis, dolor ad dolore laboriosam, labore veniam quibusdam molestiae fugiat vel alias possimus, animi officiis!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti modi vitae accusamus excepturi culpa ex nihil recusandae, dolores similique voluptate quae quidem corporis atque eos. Nesciunt minus perspiciatis beatae ipsa?
+        </p>
+        <h2>Lorem Ipsum</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem voluptatem aspernatur ratione temporibus voluptatum quod optio architecto magnam corrupti quibusdam. Excepturi mollitia est vitae magni maxime expedita minima sed eveniet.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias excepturi nulla earum ipsa exercitationem quod. Sit sint quidem, aspernatur tempore id in facilis possimus necessitatibus fuga exercitationem architecto neque mollitia?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veniam dolor consectetur sit consequatur iusto nostrum a quis corrupti culpa corporis, minus dolorem excepturi ipsam reprehenderit maiores illo mollitia reiciendis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui culpa sunt vero ad quaerat tempora, a nostrum. Illum, ea voluptate soluta eum corporis nam labore obcaecati quos aliquam repellendus deserunt.
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut qui laudantium esse voluptas earum est perferendis, dolor ad dolore laboriosam, labore veniam quibusdam molestiae fugiat vel alias possimus, animi officiis!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti modi vitae accusamus excepturi culpa ex nihil recusandae, dolores similique voluptate quae quidem corporis atque eos. Nesciunt minus perspiciatis beatae ipsa?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem voluptatem aspernatur ratione temporibus voluptatum quod optio architecto magnam corrupti quibusdam. Excepturi mollitia est vitae magni maxime expedita minima sed eveniet.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias excepturi nulla earum ipsa exercitationem quod. Sit sint quidem, aspernatur tempore id in facilis possimus necessitatibus fuga exercitationem architecto neque mollitia?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veniam dolor consectetur sit consequatur iusto nostrum a quis corrupti culpa corporis, minus dolorem excepturi ipsam reprehenderit maiores illo mollitia reiciendis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui culpa sunt vero ad quaerat tempora, a nostrum. Illum, ea voluptate soluta eum corporis nam labore obcaecati quos aliquam repellendus deserunt.
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut qui laudantium esse voluptas earum est perferendis, dolor ad dolore laboriosam, labore veniam quibusdam molestiae fugiat vel alias possimus, animi officiis!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti modi vitae accusamus excepturi culpa ex nihil recusandae, dolores similique voluptate quae quidem corporis atque eos. Nesciunt minus perspiciatis beatae ipsa?
+        </p>
+        
     </section>
 
     <?php
         include "assets/iframes/footer.html";
     ?>
 
-    <script src="assets/scripts/random/random-background.js"></script>
     <script src="assets/scripts/random/random-phrase.js"></script>
 </body>
 </html>
---
+++ admin/publicacoes/index.php
@@ -0,0 +1,35 @@
+<?php
+    include '../security.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área do Administrador</title>
+    <link rel="stylesheet" href="../assets/style/default.css">
+    <link rel="stylesheet" href="../assets/style/head-nav-foot.css">
+</head>
+<body>
+    <?php
+        include "../assets/iframes/header.html";
+        include "../assets/iframes/nav_in.html";
+    ?>
+
+    <section>
+        <h1>Publicações</h1>
+        <h2>O que essa tabela contém?</h2> 
+        <p>
+            Essa página reúne todos os livros, 
+        </p>
+
+    </section>
+
+    <?php
+        include "../assets/iframes/footer.html";
+    ?>
+
+    <script src="../assets/scripts/random/random-phrase.js"></script>
+</body>
+</html>
